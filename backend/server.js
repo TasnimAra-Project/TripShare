42084260a067e0367d0e84ca11f466b5825a43f3
shahsabin11 <shahs@lawrence.edu>
2025-08-05 14:47:17 -0500

    Setting up the login structure

--- backend/server.js
+++ backend/server.js
@@ -59,6 +59,41 @@ app.post('/api/register', async (req, res) => {
 
 });
 
+// Login route
+app.post('/api/login', (req, res) => {
+    const {username, password } = req.body;
+
+    if(!username || !password) {
+        return res.status(400).json({message: ' Please provide username and password'});
+
+    }
+
+    const query = 'SELECT * FROM users WHERE username = ?';
+    db.query(query, [username], async(err, results) => {
+        if(err) {
+            console.error('DB error during login:', err);
+            return res.status(500).json({message: 'Database error'});
+        }
+        if(results.length === 0) {
+            return res.status(401).json({message: 'Invalid username or password'});
+        }
+
+        const user = results [0];
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch) {
+            return res.status(401).json({message: 'Invalid username or password'});
+        }
+        const userData = {
+            id: user.id,
+            username: user.username,
+            email: user.email,
+            phone: user.phone
+        };
+        return res.status(200).json({success: true, message: 'Login successul!', user: userData});
+    });
+});
+
 app.listen(PORT, () => {
     console.log(`Server running on http://localhost:${PORT}`);
 })
